adc67686677e326bfd5cc40db42412f9e90e2980
detvdl <detlev.vandaele@gmail.com>
2017-10-04 11:54:02 +0200

    Use ripgrep as default projectile grep

--- modules/util/detvdl-projectile.el
+++ modules/util/detvdl-projectile.el
@@ -8,5 +8,10 @@
   (setq projectile-completion-system 'ivy)
   (projectile-mode))
 
+(use-package projectile-ripgrep
+  :ensure t
+  :bind (:map projectile-mode-map
+         ("C-c p s s" . projectile-ripgrep)))
+
 (provide 'detvdl-projectile)
 ;;; detvdl-projectile.el ends here
