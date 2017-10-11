60d738de04b577fa351f6bbeabbe6eb3b8e1ec82
detvdl <detlev.vandaele@gmail.com>
2017-10-11 18:06:04 +0200

    make sure to load sml *after* theme

--- modules/util/detvdl-theme.el
+++ modules/util/detvdl-theme.el
@@ -14,9 +14,11 @@
 (use-package smart-mode-line
   :ensure t
   :config
-  (progn
-    (setq sml/no-confirm-load-theme t)
-    (sml/setup)))
+  (with-eval-after-load 'doom-themes
+    (progn
+      (setq sml/no-confirm-load-theme t
+            sml/theme 'dark)
+      (sml/setup))))
 
 (use-package plan9-theme
   :ensure t
