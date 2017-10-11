5439cb7119cee81a8ffa21451cbd7584f59b40e1
detvdl <detlev.vandaele@gmail.com>
2017-10-11 22:27:29 +0200

    JDK 1.9 is actually JDK 9 now

--- modules/lang/detvdl-java.el
+++ modules/lang/detvdl-java.el
@@ -9,7 +9,8 @@
   :config
   (progn
     (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir))
-    (add-hook 'java-mode-hook #'jdee-mode)))
+    (add-hook 'java-mode-hook #'jdee-mode)
+    (setq jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin")))))
 
 (provide 'detvdl-java)
 ;;; detvdl-java.el ends here
