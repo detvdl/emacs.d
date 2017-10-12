18cc325ded0a807a7082f59b533e75131b6a8294
detvdl <detlev.vandaele@gmail.com>
2017-10-12 19:54:29 +0200

    persp-mode is broken on emacs-26, use perspeen

--- modules/util/detvdl-persp.el
+++ modules/util/detvdl-persp.el
@@ -2,45 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package persp-mode
+(use-package perspeen
   :ensure t
   :init
-  (setq-default persp-keymap-prefix (kbd "C-c n"))
+  (setq perspeen-use-tab nil)
   :config
-  (progn
-    (setq persp-nil-name "main"
-          persp-auto-resume-time -1
-          persp-auto-save-num-of-backups 1
-          persp-autokill-buffer-on-remove 'kill-weak
-          persp-save-dir emacs-persp-dir)
-
-    (with-eval-after-load "ivy"
-      (add-hook 'ivy-ignore-buffers
-                #'(lambda (b)
-                    (when persp-mode
-                      (let ((persp (get-current-persp)))
-                        (if persp
-                            (not (persp-contain-buffer-p b persp))
-                          nil)))))
-      (setq ivy-sort-functions-alist
-            (append ivy-sort-functions-alist
-                    '((persp-kill-buffer   . nil)
-                      (persp-remove-buffer . nil)
-                      (persp-add-buffer    . nil)
-                      (persp-switch        . nil)
-                      (persp-window-switch . nil)
-                      (persp-frame-switch  . nil)))))
-    ;; automatically create `notmuch' perspective when mode is activated
-    (with-eval-after-load "notmuch"
-      (persp-def-auto-persp "notmuch"
-                            :parameters '((dont-save-to-file . t))
-                            :mode-name ".*notmuch.*"
-                            :dyn-env '(after-change-major-mode-functions ;; prevent recursion
-                                       (persp-add-buffer-on-find-file nil)
-                                       persp-add-buffer-on-after-change-major-mode)
-                            :hooks '(change-major-mode-hook)
-                            :switch 'frame))
-    (persp-mode 1)))
+  (perspeen-mode))
 
 (provide 'detvdl-persp)
 ;;; detvdl-persp.el ends here
