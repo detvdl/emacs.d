8132b5d749a429f8284a68a327f2c521f9a0f367
detvdl <detlev.vandaele@gmail.com>
2017-10-15 15:21:58 +0200

    assume we'll always follow prezto-file symlinks

--- modules/lang/detvdl-shell.el
+++ modules/lang/detvdl-shell.el
@@ -7,19 +7,21 @@
   :defer t
   :mode (("\\.sh\\'" . shell-script-mode)
          ("\\.zsh\\'" . shell-script-mode)
-         ("\\zlogin\\'" . shell-script-mode)
-         ("\\zlogin\\'" . shell-script-mode)
-         ("\\zlogout\\'" . shell-script-mode)
-         ("\\zpreztorc\\'" . shell-script-mode)
-         ("\\zprofile\\'" . shell-script-mode)
-         ("\\zshenv\\'" . shell-script-mode)
-         ("\\zshrc\\'" . shell-script-mode)))
+         ("zlogin\\'" . shell-script-mode)
+         ("zlogin\\'" . shell-script-mode)
+         ("zlogout\\'" . shell-script-mode)
+         ("zpreztorc\\'" . shell-script-mode)
+         ("zprofile\\'" . shell-script-mode)
+         ("zshenv\\'" . shell-script-mode)
+         ("zshrc\\'" . shell-script-mode)))
 
-;; (add-hook 'sh-mode-hook
-;;           (lambda ()
-;;             (if (and buffer-file-name
-;;                      (member (file-name-nondirectory buffer-file-name) prelude-prezto-files))
-;;                 (sh-set-shell "zsh"))))
+(defvar prezto-files '("zlogin" "zlogout" "zpreztorc" "zprofile" "zshenv" "zshrc"))
+
+(add-hook 'sh-mode-hook
+          (lambda ()
+            (if (and buffer-file-name
+                     (member (file-name-nondirectory buffer-file-name) prezto-files))
+                (sh-set-shell "zsh"))))
 
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
