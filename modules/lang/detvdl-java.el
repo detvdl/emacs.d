d4da8e22903699cc13c92de241ccb6ee85c8e8a5
detvdl <detlev.vandaele@gmail.com>
2017-10-18 10:40:33 +0200

    it's time to stop

--- modules/lang/detvdl-java.el
+++ modules/lang/detvdl-java.el
@@ -2,16 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (use-package jdee
-;;   :ensure t
-;;   :mode ("\\.java\\'" . jdee-mode)
-;;   :init
-;;   (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir)
-;;         jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home"))
-;;         jdee-jdk '("1.9"))
-;;   :config
-;;   (use-package memoize
-;;     :ensure t))
+(message "Just stop trying to make emacs work with Java.")
 
 (provide 'detvdl-java)
 ;;; detvdl-java.el ends here
