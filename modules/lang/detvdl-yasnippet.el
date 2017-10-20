b68c88e184b63168288315fa6395b964971b4542
detvdl <detlev.vandaele@gmail.com>
2017-10-20 17:30:14 +0200

    yasnippet with every company backend wasn't THAT easy

--- modules/lang/detvdl-yasnippet.el
+++ modules/lang/detvdl-yasnippet.el
@@ -8,7 +8,14 @@
   :config
   (progn
     (with-eval-after-load "company"
-      (setq company-backends '(company-yasnippet)))
+      (defvar company-mode/enable-yas t)
+      (defun company-mode/backend-with-yas (backend)
+        (if (or (not company-mode/enable-yas)
+                (and (listp backend) (member 'company-yasnippet backend)))
+            backend
+          (append (if (consp backend) backend (list backend))
+                  '(:with company-yasnippet))))
+      (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
     (yas-global-mode 1)))
 
 (provide 'detvdl-yasnippet)
