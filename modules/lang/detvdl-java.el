5ff5658cf81967c70052879d5b48cec0cc27a4df
detvdl <detlev.vandaele@gmail.com>
2017-10-20 17:32:23 +0200

    to boldly go where no man has been before (java)

--- modules/lang/detvdl-java.el
+++ modules/lang/detvdl-java.el
@@ -2,7 +2,32 @@
 ;;; Commentary:
 ;;; Code:
 
-(message "Just stop trying to make emacs work with Java.")
+(use-package eclim
+  :ensure t
+  :defer t
+  :init
+  (setq-default eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse")
+                eclimd-executable "/Applications/Eclipse.app/Contents/Eclipse/plugins/org.eclim_2.7.0/bin/eclimd"
+                eclim-executable "/Applications/Eclipse.app/Contents/Eclipse/plugins/org.eclim_2.7.0/bin/eclim")
+  :config
+  (progn
+    (setq eclimd-autostart nil
+          eclim-print-debug-messages t
+          ;; Show compilation errors in minibuffer
+          help-at-pt-display-when-idle t
+          help-at-pt-timer-delay 0.1)
+    (help-at-pt-set-timer)
+    (add-hook 'java-mode-hook (lambda ()
+                                (message "Just stop trying to make emacs work with Java.")
+                                (eclim-mode t)))))
+
+(use-package company-emacs-eclim
+  :ensure t
+  :defer t
+  :config
+  (with-eval-after-load "company"
+    (company-emacs-eclim-setup)
+    (setq company-emacs-eclim-ignore-case t)))
 
 (provide 'detvdl-java)
 ;;; detvdl-java.el ends here
