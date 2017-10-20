4869f09b290b36b23477d4dde4ed92e9803e87ee
detvdl <detlev.vandaele@gmail.com>
2017-10-20 17:34:25 +0200

    squash! use rainbow delimiters globally

--- modules/lang/detvdl-clojure.el
+++ modules/lang/detvdl-clojure.el
@@ -7,8 +7,7 @@
 (use-package clojure-mode
   :ensure t
   :config
-  (add-hook 'clojure-mode-hook #'subword-mode)
-  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
+  (add-hook 'clojure-mode-hook #'subword-mode))
 
 (use-package cider
   :ensure t
@@ -18,8 +17,7 @@
     (setq nrepl-log-messages t)
     (add-hook 'cider-mode-hook #'subword-mode)
     (add-hook 'cider-mode-hook #'eldoc-mode)
-    (add-hook 'cider-repl-mode-hook #'eldoc-mode)
-    (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)))
+    (add-hook 'cider-repl-mode-hook #'eldoc-mode)))
 
 (provide 'detvdl-clojure)
 ;;; detvdl-clojure.el ends here
