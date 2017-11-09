10173ee2423605ae20dd33380e61bfb0e49e3806
detvdl <detlev.vandaele@gmail.com>
2017-11-09 15:29:57 +0100

    Revert "yasnippet with every company backend wasn't THAT easy"
    This reverts commit b68c88e184b63168288315fa6395b964971b4542.

--- modules/lang/detvdl-yasnippet.el
+++ modules/lang/detvdl-yasnippet.el
@@ -8,14 +8,7 @@
   :config
   (progn
     (with-eval-after-load "company"
-      (defvar company-mode/enable-yas t)
-      (defun company-mode/backend-with-yas (backend)
-        (if (or (not company-mode/enable-yas)
-                (and (listp backend) (member 'company-yasnippet backend)))
-            backend
-          (append (if (consp backend) backend (list backend))
-                  '(:with company-yasnippet))))
-      (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
+      (setq company-backends '(company-yasnippet)))
     (yas-global-mode 1)))
 
 (provide 'detvdl-yasnippet)
