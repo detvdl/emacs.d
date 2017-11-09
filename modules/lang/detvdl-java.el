144773a296e0cb0a0fc380757380fa8d9dc64e3b
detvdl <detlev.vandaele@gmail.com>
2017-11-09 15:29:59 +0100

    Revert "try out emacs-eclim"
    This reverts commit 1678224f72e1ad37f196aa50dd4ec29c0119d218.

--- modules/lang/detvdl-java.el
+++ modules/lang/detvdl-java.el
@@ -2,34 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (use-package jdee
-;;   :ensure t
-;;   :mode ("\\.java\\'" . jdee-mode)
-;;   :init
-;;   (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir)
-;;         jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home"))
-;;         jdee-jdk '("1.9"))
-;;   :config
-;;   (use-package memoize
-;;     :ensure t))
-
-(use-package eclim
+(use-package jdee
   :ensure t
-  :mode ("\\.java\\'" . eclim-mode)
-  :defer t
+  :mode ("\\.java\\'" . jdee-mode)
+  :init
+  (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir)
+        jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home"))
+        jdee-jdk '("1.9"))
   :config
-  (progn
-    (setq eclim-eclipse-dirs '("/Users/detlev/.eclipse/java-oxygen/Eclipse.app/Contents/Eclipse")
-          eclim-executable "/Users/detlev/.eclipse/java-oxygen/Eclipse.app/Contents/Eclipse/eclim"
-          eclimd-autostart t)))
-
-(use-package company-emacs-eclim
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (company-emacs-eclim-setup)
-    (setq company-emacs-eclim-ignore-case t)))
+  (use-package memoize
+    :ensure t))
 
 (provide 'detvdl-java)
 ;;; detvdl-java.el ends here
