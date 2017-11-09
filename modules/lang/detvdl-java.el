ab27671a6ebb66c113b0ec19eaf5e475c4446a99
detvdl <detlev.vandaele@gmail.com>
2017-11-09 15:30:05 +0100

    Revert "JDEE is a bit of a mess"
    This reverts commit 9808cb2d38cf2e5bdd903903449ad9d54a656f24.

--- modules/lang/detvdl-java.el
+++ modules/lang/detvdl-java.el
@@ -4,14 +4,13 @@
 
 (use-package jdee
   :ensure t
-  :mode ("\\.java\\'" . jdee-mode)
   :init
-  (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir)
-        jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home"))
-        jdee-jdk '("1.9"))
+  (use-package memoize :ensure t)
   :config
-  (use-package memoize
-    :ensure t))
+  (progn
+    (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir))
+    (add-hook 'java-mode-hook #'jdee-mode)
+    (setq jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin")))))
 
 (provide 'detvdl-java)
 ;;; detvdl-java.el ends here
