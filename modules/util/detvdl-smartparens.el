91f5befbe1c6e528f8864bed30aa9bf0e052673e
detvdl <detlev.vandaele@gmail.com>
2017-11-09 15:30:04 +0100

    Revert "alway highlight matching parens"
    This reverts commit 700607c911fc031d87219777df39a565e81a5cf4.

--- modules/util/detvdl-smartparens.el
+++ modules/util/detvdl-smartparens.el
@@ -23,7 +23,5 @@
     (add-hook 'prog-mode-hook (lambda ()
                                 (smartparens-mode +1)))))
 
-(show-paren-mode 1)
-
 (provide 'detvdl-smartparens)
 ;;; detvdl-smartparens.el ends here
