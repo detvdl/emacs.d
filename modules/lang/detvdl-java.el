6ac42b20a80f08c64ad54a82c69c71e1019a1def
detvdl <detlev.vandaele@gmail.com>
2017-11-09 15:29:58 +0100

    Revert "it's time to stop"
    This reverts commit d4da8e22903699cc13c92de241ccb6ee85c8e8a5.

--- modules/lang/detvdl-java.el
+++ modules/lang/detvdl-java.el
@@ -2,7 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
-(message "Just stop trying to make emacs work with Java.")
+;; (use-package jdee
+;;   :ensure t
+;;   :mode ("\\.java\\'" . jdee-mode)
+;;   :init
+;;   (setq jdee-server-dir (expand-file-name "jdee-server" emacs-dir)
+;;         jdee-jdk-registry '(("1.9" . "/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home"))
+;;         jdee-jdk '("1.9"))
+;;   :config
+;;   (use-package memoize
+;;     :ensure t))
 
 (provide 'detvdl-java)
 ;;; detvdl-java.el ends here
