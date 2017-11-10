7a7c82e3d9004b0accd2be78cfb078669e6ba3fd
detvdl <detlev.vandaele@gmail.com>
2017-11-10 15:31:23 +0100

    why did I install this when I have counsel-rg?

--- modules/util/detvdl-projectile.el
+++ modules/util/detvdl-projectile.el
@@ -17,11 +17,5 @@
                        '(("g" magit-status "magit status"))))
     (projectile-mode)))
 
-(use-package projectile-ripgrep
-  :ensure t
-  :after projectile
-  :bind (:map projectile-mode-map
-              ("C-c p s s" . projectile-ripgrep)))
-
 (provide 'detvdl-projectile)
 ;;; detvdl-projectile.el ends here
