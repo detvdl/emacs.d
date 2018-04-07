be8d53077a9a873b614efbbcd543af51172a2ae0
detvdl <detlev.vandaele@gmail.com>
2018-04-07 22:36:48 +0200

    No longer using mail nor perpectives

--- modules/util/detvdl-util.el
+++ modules/util/detvdl-util.el
@@ -6,8 +6,6 @@
 (require 'detvdl-ivy)
 (require 'detvdl-projectile)
 (require 'detvdl-git)
-;; (require 'detvdl-mail)
-;; (require 'detvdl-persp)
 (require 'detvdl-history)
 (require 'detvdl-theme)
 (require 'detvdl-erc)
