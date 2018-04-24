59b7f220429789bc9211d145e94858b0f039f508
detvdl <detlev.vandaele@gmail.com>
2018-04-24 20:42:07 +0200

    Default frame and divider settings

--- core/detvdl-ui.el
+++ core/detvdl-ui.el
@@ -1,7 +1,6 @@
 ;;; detvdl-ui.el --- Test
 ;;; Commentary:
 ;;; Code:
-
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 
@@ -11,11 +10,7 @@
 
 (setq inhibit-splash-screen t
       initial-scratch-message nil
-      initial-major-mode 'org-mode)
-
-;; turning menu-bar-mode off messes with OS X' (cocoa)
-;; native menu-bar integration
-;; (menu-bar-mode -1)
+      initial-major-mode 'lisp-interaction-mode)
 
 (blink-cursor-mode -1)
 
@@ -33,24 +28,43 @@
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
 
-;; mode line settings
-;; smart-mode-line setup in modules/util/detvdl-theme.el
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-(diminish 'size-indication-mode)
+;; Window-dividers
+(window-divider-mode t)
+(setq window-divider-default-right-width 2)
+(let ((wd--fg (face-attribute 'mode-line-inactive :background)))
+  (set-face-attribute 'window-divider nil :foreground wd--fg)
+  (set-face-attribute 'window-divider-first-pixel nil :foreground wd--fg)
+  (set-face-attribute 'window-divider-last-pixel nil :foreground wd--fg))
 
 ;; enable y/n answers
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(defun set-font (font-str)
+;; Frame settings (position, size)
+(defvar detvdl:default-frame-size '(1720 1060))
+(defvar detvdl:default-screen-size '(1900 1194))
+
+(defun detvdl:scale-and-center-frame (&optional frame)
+  "Scale FRAME according to `detvdl:default-frame-size' and center it."
+  (let ((frame (or frame (selected-frame))))
+    (when window-system
+      (pcase-let* ((`(,fwidth ,fheight) detvdl:default-frame-size)
+                   (`(,swidth ,sheight) detvdl:default-screen-size))
+        (progn
+          (set-frame-size frame fwidth fheight t)
+          (set-frame-position frame
+                              (/ (- swidth fwidth) 2)
+                              (/ (- sheight fheight) 2)))))))
+;; Font settings
+(defun detvdl:set-font (font-str)
   "Set the default font to the FONT-STR parameter."
   (add-to-list 'default-frame-alist `(font . ,font-str))
   (set-face-attribute 'default t :font font-str)
   (when (window-system)
     (set-frame-font font-str)))
 
-(set-font "Fira Code Retina-12")
+;; Call utility functions here
+(detvdl:scale-and-center-frame)
+(detvdl:set-font "Fira Code Retina-12")
 
 (provide 'detvdl-ui)
 ;;; detvdl-ui.el ends here
