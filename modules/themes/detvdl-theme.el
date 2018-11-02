26ac79901f9f3a4adfb7b18d8555b8b843ada2c6
detvdl <detlev.vandaele@gmail.com>
2018-11-02 17:43:59 +0100

    Yes, yes, themes again

--- modules/themes/detvdl-theme.el
+++ modules/themes/detvdl-theme.el
@@ -3,7 +3,6 @@
 ;;; Code:
 
 (require 'detvdl-modeline)
-(require 'ligatures-font)
 (require 'detvdl-ui)
 
 (defvar detvdl:after-theme-fns '())
@@ -12,26 +11,15 @@
   :ensure t
   :defer t
   :init
-  (defun after-load-base16-default-dark ()
-    (custom-theme-set-faces
-     'base16-default-dark
-     '(fringe ((t (:background "#202024"))))
-     '(window-divider ((t (:foreground "grey50"))))))
-  (defun after-load-base16-tomorrow-night ()
-    (custom-theme-set-faces
-     'base16-tomorrow-night
-     '(fringe ((t (:background "#2D2E32"))))
-     '(window-divider ((t (:foreground "grey50"))))
-     ))
   (defun after-load-base16-tomorrow-black ()
     (custom-theme-set-faces
      'base16-tomorrow-black
      '(default ((nil (:background "#0b0c0c" :foreground "#d0d0d0"))))
      '(line-number ((t (:background "#0b0b0c" :foreground "#3b3b3b"))))
      '(line-number-current-line ((t (:background "#2b2b2b" :foreground "#d0d0d0"))))
-     '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#111111"))))
+     ;; '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#111111"))))
      '(region ((nil (:background "#464740"))))
-     '(hl-line ((nil (:background "#202020"))))
+     ;; '(hl-line ((nil (:background "#202020"))))
      '(yas-field-highlight-face ((nil (:background "#333399"))))
      '(js2-function-param-face ((t (:foreground "LightGoldenrod"))))
      '(font-lock-warning-face ((nil (:foreground "#ff6666"))))
@@ -41,58 +29,74 @@
      '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#cccccc" :background "gray20"))))
      '(org-block ((t (:background "gray10" :foreground "#de935f"))))
      '(org-code ((t (:foreground "#8abeb7" :weight bold))))
-     )
-    )
-  (add-to-list 'detvdl:after-theme-fns '(base16-default-dark . after-load-base16-default-dark))
-  (add-to-list 'detvdl:after-theme-fns '(base16-tomorrow-night . after-load-base16-tomorrow-night))
-  (add-to-list 'detvdl:after-theme-fns '(base16-tomorrow-black . after-load-base16-tomorrow-black))
-  )
-
-(use-package farmhouse-theme
-  :load-path "modules/themes/custom-themes/emacs-farmhouse-theme"
-  ;; :ensure t
-  :defer t
-  :init
-  (defun after-load-farmhouse ()
-    (custom-theme-set-faces
-     'farmhouse-light
-     ;; '(cursor ((t (:background "grey40"))))
-     '(font-lock-warning-face ((t (:foreground "#ec3691" :weight bold))))
-     ;; '(font-lock-constant-face ((t (:foreground "#7750BA"))))
-
-     '(font-latex-sectioning-5-face ((t (:inherit variable-pitch :foreground "purple" :weight bold))))
-     '(font-latex-math-face ((t (:foreground "#A6825B"))))
-     '(font-latex-warning-face ((t (:foreground "firebrick1"))))
-     '(font-latex-sedate-face ((t (:foreground "gray30"))))
-
-     '(cider-result-overlay-face ((t (:background "gray90"))))
-     '(cider-deprecated-face ((t (:background "#FFAF5E"))))
-
-     '(indent-guide-face ((t (:foreground "#ccc"))))
-     '(hl-line ((t (:foreground nil :background "#F5F0ED"))))
-
-     '(window-divider ((t (:foreground "grey50"))))
-     `(line-number ((t (:background ,(face-attribute 'default :background)
-                        :foreground "#b2b2b2"))))
-     `(line-number-current-line ((t (:background ,(face-attribute 'hl-line :background)
-                                     :foreground ,(face-attribute 'default :foreground)))))
-     '(fringe ((t (:background "grey85"))))
      ))
-  (add-to-list 'detvdl:after-theme-fns '(farmhouse-light . after-load-farmhouse))
-  )
+  (add-to-list 'detvdl:after-theme-fns '(base16-tomorrow-black . after-load-base16-tomorrow-black)))
+
+;; (use-package farmhouse-theme
+;;   :load-path "modules/themes/custom-themes/emacs-farmhouse-theme"
+;;   :defer t
+;;   :init
+;;   (defun after-load-farmhouse ()
+;;     (custom-theme-set-faces
+;;      'farmhouse-light
+;;      '(font-lock-warning-face ((t (:foreground "#ec3691" :weight bold))))
+;;      '(font-latex-sectioning-5-face ((t (:inherit variable-pitch :foreground "purple" :weight bold))))
+;;      '(font-latex-math-face ((t (:foreground "#A6825B"))))
+;;      '(font-latex-warning-face ((t (:foreground "firebrick1"))))
+;;      '(font-latex-sedate-face ((t (:foreground "gray30"))))
+;;      '(cider-result-overlay-face ((t (:background "gray90"))))
+;;      '(cider-deprecated-face ((t (:background "#FFAF5E"))))
+;;      '(indent-guide-face ((t (:foreground "#ccc"))))
+;;      '(window-divider ((t (:foreground "grey50"))))
+;;      `(line-number ((t (:background ,(face-attribute 'default :background)
+;;                         :foreground "#b2b2b2"))))
+;;      `(line-number-current-line ((t (:background ,(face-attribute 'hl-line :background)
+;;                                      :foreground ,(face-attribute 'default :foreground)))))
+;;      '(fringe ((t (:background "grey85"))))
+;;      ))
+;;   (add-to-list 'detvdl:after-theme-fns '(farmhouse-light . after-load-farmhouse)))
+
+(use-package solarized-theme
+  :ensure t
+  :init
+  (setq solarized-distinct-fringe-background t
+        solarized-emphasize-indicators t
+        solarized-scale-org-headlines nil
+        solarized-distinct-doc-face t
+        solarized-height-minus-1 1.0
+        solarized-height-plus-1 1.0
+        solarized-height-plus-2 1.0
+        solarized-height-plus-3 1.0
+        solarized-height-plus-4 1.0)
+  (defun after-solarized-light ()
+    (set-face-attribute 'default nil :foreground "#666666")
+    (set-face-attribute 'line-number-current-line nil :weight 'bold)
+    (set-face-attribute 'line-number-current-line nil :foreground "#999999")
+    (set-face-attribute 'line-number nil :weight 'regular)
+    (set-face-attribute 'line-number nil :foreground "#bbbbbb"))
+  (add-to-list 'detvdl:after-theme-fns '(solarized-light . after-solarized-light))
+  :defer t)
+
+(use-package gruvbox-theme
+  :ensure t
+  :defer t)
+
+(use-package tao-theme
+  :ensure t
+  :defer t)
+
+(use-package nofrils-acme-theme
+  :ensure t
+  :defer t)
 
 ;; Window-dividers
 (setq window-divider-default-right-width 4
-      window-divider-default-bottom-width 0)
+      window-divider-default-bottom-width 2)
 (window-divider-mode t)
 
-(use-package all-the-icons
-  :ensure t
-  :demand)
-
-(defvar detvdl:themes (list :light 'farmhouse-light
+(defvar detvdl:themes (list :light 'solarized-light
                             :dark 'base16-tomorrow-black))
-(defvar detvdl:default-theme 'base16-tomorrow-black)
+(defvar detvdl:default-theme 'solarized-light)
 (defvar detvdl:current-theme nil)
 (defvar detvdl:default-after-theme-fns '(detvdl:extra-typeface-settings
                                          detvdl:set-modeline))
@@ -113,7 +117,7 @@ These are merged with `detvdl:default-after-theme-fns'"
                       "Choose:"
                       (custom-available-themes)
                       nil t))
-                  (or theme (detvd:default-theme))))
+                  (or theme (detvdl:default-theme))))
          (type (if (eq (plist-get detvdl:themes :light) theme)
                    :light
                  :dark))
@@ -131,14 +135,15 @@ These are merged with `detvdl:default-after-theme-fns'"
       (mapc (lambda (fn) (when fn (funcall fn))) after-fns))))
 
 (if (daemonp)
-    (add-hook 'after-make-frame-functions (lambda () (detvdl:load-theme (plist-get detvdl:themes :dark))))
+    (add-hook 'after-make-frame-functions
+              (lambda () (detvdl:load-theme (plist-get detvdl:themes :light))))
   (if (display-graphic-p)
-      (detvdl:load-theme (plist-get detvdl:themes :dark))
+      (detvdl:load-theme (plist-get detvdl:themes :light))
     (detvdl:load-theme (plist-get detvdl:themes :light))
     ))
 
-(add-hook 'prog-mode-hook (lambda () (progn (my:ligatures-fira-code-setup)
-                                            (prettify-symbols-mode 1))))
+;; (add-hook 'prog-mode-hook (lambda () (progn (my:ligatures-fira-code-setup)
+;;                                             (prettify-symbols-mode 1))))
 
 (defun detvdl:toggle-theme (&optional frame)
   "Toggle between light and dark theme in the current FRAME."
