a8cfed5dd64a9fd7df0d552b3d090c43fd821e7a
detvdl <detlev.vandaele@gmail.com>
2019-01-03 21:25:04 +0100

    Add describe-thing-at-point function

--- init.el
+++ init.el
@@ -731,10 +731,38 @@ Lisp function does not specify a special indentation."
         sr-speedbar-default-width 30
         sr-speedbar-width 30))
 
+;;;; Describe thing at point
+;; handy function from https://www.emacswiki.org/emacs/DescribeThingAtPoint
+(defun describe-thing-at-point ()
+  "Show the documentation of the Elisp function and variable near point.
+This checks in turn:
+-- for a function name where point is
+-- for a variable name where point is
+-- for a surrounding function call"
+  (interactive)
+  (let (sym)
+	;; sigh, function-at-point is too clever.  we want only the first half.
+	(cond ((setq sym (ignore-errors
+                       (with-syntax-table emacs-lisp-mode-syntax-table
+                         (save-excursion
+                           (or (not (zerop (skip-syntax-backward "_w")))
+                               (eq (char-syntax (char-after (point))) ?w)
+                               (eq (char-syntax (char-after (point))) ?_)
+                               (forward-sexp -1))
+                           (skip-chars-forward "`'")
+        	               (let ((obj (read (current-buffer))))
+                             (and (symbolp obj) (fboundp obj) obj))))))
+           (describe-function sym))
+          ((setq sym (variable-at-point)) (describe-variable sym))
+          ;; now let it operate fully -- i.e. also check the
+          ;; surrounding sexp for a function call.
+          ((setq sym (function-at-point)) (describe-function sym)))))
+
 ;;;; Xref
 (bind-key "M-." 'xref-find-definitions prog-mode-map)
 (bind-key "M-," 'xref-pop-marker-stack prog-mode-map)
 (bind-key "M-[" 'xref-find-references prog-mode-map)
+(bind-key "M-]" 'describe-thing-at-point prog-mode-map)
 
 ;;; Git
 ;;;; Magit
