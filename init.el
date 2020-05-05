d1c535fe25eecf0c516aca3197ea94a789f46c4c
Detlev Vandaele & Lander Vandergucht <detlev.vandaele+lander.vandergucht@tomtom.com>
2020-05-05 16:01:48 +0200

    turn on solaire-mode + remove ivy-rich code

--- init.el
+++ init.el
@@ -430,46 +430,6 @@ static char * data[] = {
   (setq ivy-posframe-width 80)
   (ivy-posframe-mode +1))
 
-;; (use-package ivy-rich
-;;   :ensure t
-;;   :delight
-;;   :preface
-;;   (defun ivy-rich-switch-buffer-icon (candidate)
-;;     (with-current-buffer
-;;         (get-buffer candidate)
-;;       (all-the-icons-icon-for-mode major-mode)))
-;;   :init
-;;   (setq ivy-rich-display-transformers-list ; max column width sum = (ivy-poframe-width - 1)
-;;         '(ivy-switch-buffer
-;;           (:columns
-;;            ((ivy-rich-switch-buffer-icon (:width 2))
-;;             (ivy-rich-candidate (:width 35))
-;;             (ivy-rich-switch-buffer-project (:width 15 :face success))
-;;             (ivy-rich-switch-buffer-major-mode (:width 13 :face warning)))
-;;            :predicate
-;;            (lambda (cand) (get-buffer cand)))
-;;           counsel-M-x
-;;           (:columns
-;;            ((counsel-M-x-transformer (:width 35))
-;;             (ivy-rich-counsel-function-docstring (:width 34 :face font-lock-doc-face))))
-;;           counsel-describe-function
-;;           (:columns
-;;            ((counsel-describe-function-transformer (:width 35))
-;;             (ivy-rich-counsel-function-docstring (:width 34 :face font-lock-doc-face))))
-;;           counsel-describe-variable
-;;           (:columns
-;;            ((counsel-describe-variable-transformer (:width 35))
-;;             (ivy-rich-counsel-variable-docstring (:width 34 :face font-lock-doc-face))))
-;;           package-install
-;;           (:columns
-;;            ((ivy-rich-candidate (:width 25))
-;;             (ivy-rich-package-version (:width 12 :face font-lock-comment-face))
-;;             (ivy-rich-package-archive-summary (:width 7 :face font-lock-builtin-face))
-;;             (ivy-rich-package-install-summary (:width 23 :face font-lock-doc-face))))))
-;;   :config
-;;   (ivy-rich-mode +1)
-;;   (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line))
-
 (use-package ggtags
   :ensure t
   :config
@@ -703,6 +663,7 @@ This functions should be added to the hooks of major modes for programming."
   (treemacs-fringe-indicator-mode t)
   (add-hook 'treemacs-mode-hook (lambda ()
                                   (linum-mode -1)
+                                  (display-line-numbers-mode -1)
                                   (when (display-graphic-p)
                                     (set-window-fringes nil 00))
                                   (setq tab-width 1)
@@ -798,15 +759,15 @@ This functions should be added to the hooks of major modes for programming."
   :after treemacs projectile
   :ensure t)
 
-;; (use-package solaire-mode
-;;   :ensure t
-;;   :defer 2
-;;   :hook (((after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
-;;          (minibuffer-setup . solaire-mode-in-minibuffer))
-;;   :config
-;;   (add-hook 'change-major-mode-hook #'turn-on-solaire-mode)
-;;   (solaire-global-mode +1)
-;;   (solaire-mode-swap-bg))
+(use-package solaire-mode
+  :ensure t
+  :defer 2
+  :hook (((after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+         (minibuffer-setup . solaire-mode-in-minibuffer))
+  :config
+  (add-hook 'change-major-mode-hook #'turn-on-solaire-mode)
+  (solaire-global-mode +1)
+  (solaire-mode-swap-bg))
 
 ;; Always enable eldoc
 (global-eldoc-mode +1)
