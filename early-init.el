c665207831d913941f9f2d129d88056254785968
detvdl <detvdl.services@protonmail.com>
2025-08-16 23:49:03 +0200

    refactor: make initial frame wider

--- early-init.el
+++ early-init.el
@@ -11,8 +11,8 @@
       inhibit-startup-buffer-menu t)
 
 (dolist (variable '(initial-frame-alist default-frame-alist))
-  (set variable `((width . 160)
-                  (height . 100)
+  (set variable `((width . 180)
+                  (height . 90)
                   (horizontal-scroll-bars . nil)
                   (menu-bar-lines . 1) ; alternative to disabling `menu-bar-mode'
                   (internal-border-width . 5)
